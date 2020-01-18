871eb24521d8b0411b720a0ad6de39f732fb46b7
CuneytYilmaz <cuneyilmaz@gmail.com>
2020-01-18 16:41:27 +0300

    Updated results with useState

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
+  const [query, setQuery] = useState('reacthooks')
   const [results, setResults] = useState([])
 
   useEffect (() => {
     getResults()
-  }, [])
+  }, [query])
 
   const getResults = async () => {
     const response = await axios.get(
-      'http://hn.algolia.com/api/v1/search?query=reacthooks'
+      `http://hn.algolia.com/api/v1/search?query=${query}`
     )
 
     setResults(response.data.hits)
@@ -18,6 +19,7 @@ function App() {
 
   return (
     <>
+      <input type='text' onChange={event => setQuery(event.target.value)}></input>
       <ul>
         {results.map(result => (
             <li key={result.objectID}>
